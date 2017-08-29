6b84f5fd0d113fa745b71efc0d402852f7827407
zshcoles18 <461104069@qq.com>
2017-08-29 16:04:58 +0800

    add axios

--- src/api/search.js
+++ src/api/search.js
@@ -1,6 +1,7 @@
 import Mock from 'mockjs';
 import 'whatwg-fetch';
 import $ from 'jquery';
+import axios from 'axios';
 var searchTest = function (condition, cb) {
     Mock.mock('/searchTest', {
         'listData|1-10': [
@@ -22,13 +23,16 @@ var searchTest = function (condition, cb) {
     //         cb(responseData);
     //     }
     // );
-    $.ajax({
-        type: 'POST',
-        url: '/searchTest',
-        success: function (data) {
-            cb(data);
-        },
-        dataType: 'json'
+    // $.ajax({
+    //     type: 'POST',
+    //     url: '/searchTest',
+    //     success: function (data) {
+    //         cb(data);
+    //     },
+    //     dataType: 'json'
+    // });
+    return axios.post('/searchTest').then(response=>{
+        cb(response.data);
     });
 };
 export default searchTest;
